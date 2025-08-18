e2974c9d8b1d6b7e13d536c8b11839e723bf1424
ahdeka <ahdeka01@gmail.com>
2025-08-18 12:29:25 +0900

    refactor: migrate domain/post/post/service/PostService Class to Kotlin

---
+++ backend/src/main/java/com/back/domain/post/post/service/PostService.kt
@@ -0,0 +1,61 @@
+package com.back.domain.post.post.service
+
+import com.back.domain.member.member.entity.Member
+import com.back.domain.post.post.entity.Post
+import com.back.domain.post.post.repository.PostRepository
+import com.back.domain.post.postComment.entity.PostComment
+import lombok.RequiredArgsConstructor
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+@RequiredArgsConstructor
+class PostService {
+    private val postRepository: PostRepository? = null
+
+    fun count(): Long {
+        return postRepository!!.count()
+    }
+
+    fun write(author: Member, title: String?, content: String?): Post {
+        val post = Post(author, title, content)
+
+        return postRepository!!.save<Post>(post)
+    }
+
+    fun findById(id: Int): Optional<Post?> {
+        return postRepository!!.findById(id)
+    }
+
+    fun findAll(): MutableList<Post?> {
+        return postRepository!!.findAll()
+    }
+
+    fun modify(post: Post, title: String?, content: String?) {
+        post.modify(title, content)
+    }
+
+    fun writeComment(author: Member, post: Post, content: String?): PostComment {
+        return post.addComment(author, content)
+    }
+
+    fun deleteComment(post: Post, postComment: PostComment?): Boolean {
+        return post.deleteComment(postComment)
+    }
+
+    fun modifyComment(postComment: PostComment, content: String?) {
+        postComment.modify(content)
+    }
+
+    fun delete(post: Post) {
+        postRepository!!.delete(post)
+    }
+
+    fun findLatest(): Post? {
+        return postRepository!!.findFirstByOrderByIdDesc()
+    }
+
+    fun flush() {
+        postRepository!!.flush()
+    }
+}
